aea8d4aecc425271eed4b250317c6881f486efc9
Albert Stjärne <albert.stjarne@gmail.com>
2019-02-24 21:19:52 +0100

    adds function to close the modal on clicking close

--- main.js
+++ main.js
@@ -3,12 +3,22 @@ let modal = document.getElementById('simpleModal');
 // Get open modal button
 let modalBtn = document.getElementById('modalBtn');
 // Get close button
-let closeBtn = document.getElementById('closeBtn');
+let closeBtn = document.getElementsByClassName('closeBtn')[0];
 
 // Listen for click
 modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+
 
 // Funtion to open modal
 function openModal() {
   modal.style.display = 'block';
 }
+
+// Function to close modal
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+
